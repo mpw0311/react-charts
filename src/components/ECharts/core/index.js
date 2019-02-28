fa26038c0da077fcafc5c12133e8a701572164c2
“‘mpw <mpw0311@163.com>
2019-02-28 16:40:37 +0800

    添加属性校验

--- src/components/ECharts/core/index.js
+++ src/components/ECharts/core/index.js
@@ -42,6 +42,28 @@ export default class Chart extends PureComponent {
     }
 }
 Chart.propTypes = {
-    options: PropTypes.object,
+    //the echarts option config, can see http://echarts.baidu.com/option.html#title.
+    options: PropTypes.object.isRequired,
+    //改变图表尺寸，在容器大小发生改变时是否需要手动调用。
     isBindResize: PropTypes.bool,
+    //when setOption, not merge the data, default is false. See http://echarts.baidu.com/api.html#echartsInstance.setOption.
+    notMerge: PropTypes.bool,
+    // when setOption, lazy update the data, default is false. See http://echarts.baidu.com/api.html#echartsInstance.setOption.
+    lazyUpdate: PropTypes.bool,
+    // the style of echarts div. object, default is {height: '300px'}.
+    style: PropTypes.object,
+    //the class of echarts div. you can setting the css style of charts by class name.
+    className: PropTypes.string,
+    //the theme of echarts. string, should registerTheme before use it
+    theme: PropTypes.string,
+    //when the chart is ready, will callback the function with the echarts object as it's paramter.
+    onChartReady: PropTypes.func,
+    //the echarts loading option config, can see http://echarts.baidu.com/api.html#echartsInstance.showLoading.
+    loadingOption: PropTypes.object,
+    //bool, when the chart is rendering, show the loading mask.
+    showLoading: PropTypes.bool,
+    //binding the echarts event, will callback with the echarts event object, and the echart object as it's paramters
+    onEvents: PropTypes.object,
+    //the opts of echarts. object, will be used when initial echarts instance by echarts.init. Document here.
+    opts: PropTypes.object,
 }
