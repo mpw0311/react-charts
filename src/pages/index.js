ad74ca1a85b88500a8ff7e97af37d9be3d5c5160
“‘mpw <mpw0311@163.com>
2019-04-03 10:49:56 +0800

    bmap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import styles from './index.css';
-import { formatMessage } from 'umi-plugin-locale';
+// import { formatMessage } from 'umi-plugin-locale';
 import ChartView from '@components/ChartView';
 
 export default function () {
- 
+
   return (
     <div className={styles.normal}>
       <ul className={styles.list}>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
-            {formatMessage({ id: 'index.start' })}
+            {/* {formatMessage({ id: 'index.start' })} */}
+            <a href="bmap">百度地图</a>
           </a>
         </li>
       </ul>
