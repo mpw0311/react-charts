8fa017229207c37f1ecde8d4ea2b68bd15d744b9
“‘mpw <mpw0311@163.com>
2019-04-23 11:45:06 +0800

    echarts

--- src/pages/es6/components/async.js
+++
@@ -1,9 +0,0 @@
-export const request = async () => {
-    if (this.state.data === undefined) {
-        const data = await fetch('https://randomuser.me/api/').then(res => res.json());
-        console.log(data);
-        this.setState({
-            data
-        });
-    }
-};
