9975bd989710952b4eaaff90bc7c749bbdbd0374
“‘mpw <mpw0311@163.com>
2019-04-29 11:18:39 +0800

    hooks

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,32 +1,44 @@
+import { PureComponent } from 'react';
 import { Layout } from 'antd';
 import Menu from './menu';
 import styles from './index.less';
 import StartedModal from './startedModal';
+import Context from './context';
 import logo from '../assets/logo.png';
 const {
   Header, Sider, Content,
 } = Layout;
-function BasicLayout(props) {
-  const header = <Header className={styles.header}>
-    <span className={styles.logo}>
-      <img src={logo} alt="可视化" />
-    </span>
-    <h2 className={styles.title}>数据可视化</h2>
-  </Header>;
-  return (
-    <Layout>
-      <Layout style={{ height: '100vh' }}>
-        <Sider style={{ height: '100vh' }}>
-          {header}
-          <Menu {...props} />
-        </Sider>
-        <Content className={styles.content}>
-          {props.children}
-          <StartedModal />
-        </Content>
-      </Layout>
-    </Layout>
-  );
+
+
+class BasicLayout extends PureComponent {
+  state = {
+    theme: 'dark'
+  }
+  render() {
+    const header = <Header className={styles.header}>
+      <span className={styles.logo}>
+        <img src={logo} alt="可视化" />
+      </span>
+      <h2 className={styles.title}>数据可视化</h2>
+    </Header>;
+
+    return (
+      <Context.Provider value={{ theme: this.state.theme }}>
+        <Layout>
+          <Layout style={{ height: '100vh' }}>
+            <Sider style={{ height: '100vh' }}>
+              {header}
+              <Menu {...this.props} />
+            </Sider>
+            <Content className={styles.content}>
+              {this.props.children}
+              <StartedModal />
+            </Content>
+          </Layout>
+        </Layout>
+      </Context.Provider>
+    );
+  }
 }
 
 export default BasicLayout;
--- src/layouts/menu.js
+++ src/layouts/menu.js
@@ -3,6 +3,7 @@ import { Link } from 'umi';
 import { Menu } from 'antd';
 import Icon from '@components/Icon';
 import menuList from './menus.config';
+import Context from './context';
 import styles from './index.less';
 
 export default class extends PureComponent {
@@ -41,14 +42,18 @@ export default class extends PureComponent {
     render() {
         const { location: { pathname } } = this.props;
         return (
-            <Menu
-                defaultSelectedKeys={[pathname]}
-                mode="inline"
-                theme='dark'
-                className={styles.menu}
-            >
-                {this.renderMenu(menuList)}
-            </Menu>
+            <Context.Consumer>
+                {
+                    ({ theme }) => (<Menu
+                        defaultSelectedKeys={[pathname]}
+                        mode="inline"
+                        theme={theme}
+                        className={styles.menu}
+                    >
+                        {this.renderMenu(menuList)}
+                    </Menu>)
+                }
+            </Context.Consumer>
         );
     }
 }
